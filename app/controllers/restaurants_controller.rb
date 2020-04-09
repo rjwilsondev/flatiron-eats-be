1245c9beea23e56ad96db7b0e58b0e04c7e9fd62
cjp34 <carlparm@gmail.com>
2020-04-09 14:51:23 -0400

    Restaurants index and show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+
+    def index
+        restaurants = Restaurant.all
+        render json: restaurants.to_json(
+            :include => {:items => {:except => [:created_at, :updated_at]}
+        })
+    end
+
+    def show
+        resturant = Restaurant.find(params[:id])
+        render json: resturant.to_json(
+            :include => {:items => {:except => [:created_at, :updated_at]}
+        })
+    end
+
 end
