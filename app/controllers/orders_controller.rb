252d519410b0551a754c5e137b86de3c6ce087a2
rjwilson0531 <rjwilson0531@gmail.com>
2020-04-13 20:11:20 -0400

    orders can post after  checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,14 @@
 class OrdersController < ApplicationController
+
+    def create
+        cart = params["cart"]
+        user_id = params["user_id"].to_i
+        restaurant_id = cart[0]["restaurant_id"]
+        order = Order.create("user_id"=>user_id,"restaurant_id"=>restaurant_id)
+        
+        cart.map do |item|
+            puts item
+            OrderItem.create("item_id"=>item["id"] , "order"=>order)
+        end
+    end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
     belongs_to :user
     belongs_to :restaurant
     has_many :order_items
-    hast_many :items, through: :order_items
+    has_many :items, through: :order_items
 end
